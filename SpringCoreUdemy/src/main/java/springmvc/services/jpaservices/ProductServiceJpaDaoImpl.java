20b4bdccf5a0b2a2be96ce2e6733bcc7f7b20e88
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-29 10:06:23 -0500

    Adding more Dao implementation classes.

---
+++ SpringCoreUdemy/src/main/java/springmvc/services/jpaservices/ProductServiceJpaDaoImpl.java
@@ -0,0 +1,34 @@
+package springmvc.services.jpaservices;
+
+import java.util.List;
+
+import springmvc.domain.Product;
+import springmvc.services.ProductService;
+
+public class ProductServiceJpaDaoImpl extends AbstractJpaDaoService implements ProductService {
+
+	@Override
+	public List<?> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product saveOrUpdate(Product domainObject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
